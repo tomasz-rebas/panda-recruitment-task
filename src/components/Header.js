c6db7cf7efe2af61235ac2dd64292878e2cbd1b0
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-10-11 02:40:04 +0200

    Add header elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,22 @@ import React from 'react';
 export default function Header() {
 
     return (
-        <div></div>
+        <header>
+            <div className="topbar">
+                <button className="navbar-button">
+                </button>
+                <img 
+                    src="/logo.svg" 
+                    width="60" 
+                    height="70"
+                />
+                <img 
+                    src="/icons/shopping-cart.svg"
+                    width="50" 
+                    height="60"
+                />
+            </div>
+            <h1>Panda Collection</h1>
+        </header>
     );
 }
