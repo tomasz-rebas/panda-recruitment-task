8e7f46d673f57fd19ca958e13c36442161d8608b
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-10-11 17:27:40 +0200

    Navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default function Navigation() {
 
     return (
-        <div>
-        </div>
+        <nav>                
+            <div className="navbar-close-button">
+                <span></span>
+                <span></span>
+            </div>
+            <a href="">Home</a>
+            <a href="">Products</a>
+            <a href="">My Account</a>
+            <a href="">Customer Service</a>
+        </nav>
     );
 }
