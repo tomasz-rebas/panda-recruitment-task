128b3c5904844cba3b23b7b13c1035e48ea74dcc
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-10-11 20:31:05 +0200

    Styling for names and base prices

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,15 +3,18 @@ import './../style/ProductCard.scss';
 
 export default function ProductCard( { productData }) {
 
+    const {id, title, currency, images, prices, attributes} = productData;
+
     return (
         <div className="product-card">
             <img 
-                src={'http://localhost:3000' + productData.images.thumbnail}
+                src={'http://localhost:3000' + images.thumbnail}
                 width="216"
                 height="290"
             />
             <button className="add-to-cart-button">Add to cart</button>
-            <div>{productData.title}</div>
+            <div className="product-name">{title}</div>
+            <div className="price">{currency + prices.base}</div>
         </div>
     );
 }
