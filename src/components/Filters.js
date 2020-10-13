5e2376dbfbb1560259df79af206edbc80935c205
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-10-13 15:27:15 +0200

    Wrap checkboxes in a label tag

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -32,14 +32,16 @@ export default function Filters( { colorFilters, setColorFilters, areFiltersExpa
 
     const filterInputs = colors.map((color, index) => (
         <li key={'color-'+index}>
-            <input 
-                type="checkbox"
-                name={color.name}
-                onChange={changeFilter}
-                checked={colorFilters[color.name] ? true : false}
-            />
-            <span className={'color-preview color-preview-' + color.name}></span>
-            <span className="color-name">{color.label}</span>
+            <label>
+                <input 
+                    type="checkbox"
+                    name={color.name}
+                    onChange={changeFilter}
+                    checked={colorFilters[color.name] ? true : false}
+                />
+                <span className={'color-preview color-preview-' + color.name}></span>
+                <span className="color-name">{color.label}</span>
+            </label>
         </li>  
     ));
     
