6296b6dbb9593d9a6b9e477f13656e6ace1ddd3f
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-10-14 14:05:43 +0200

    Keep maximum of 4 products in a single row

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,16 +5,18 @@ import './../style/Products.scss';
 export default function Products( { productData, productsInCart, setProductsInCart, colorFilters }) {
 
     return (
-        <div className="products">
-            {productData.map(product => (
-                colorFilters[product.attributes.colors] &&
-                <ProductCard 
-                    productData={product}
-                    key={product.id}
-                    productsInCart={productsInCart}
-                    setProductsInCart={setProductsInCart}
-                />
-            ))}
+        <div className="products-wrapper">
+            <div className="products">
+                {productData.map(product => (
+                    colorFilters[product.attributes.colors] &&
+                    <ProductCard 
+                        productData={product}
+                        key={product.id}
+                        productsInCart={productsInCart}
+                        setProductsInCart={setProductsInCart}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
